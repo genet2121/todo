c6894f843a3740ae85b9d2cdd856c56994a34cee
Eyerusalem Tesfaye <tesfayerusalem@gmail.com>
2024-03-08 17:16:08 +0300

    merge

--- lib/screens/taskListDetail.dart
+++ lib/screens/taskListDetail.dart
@@ -22,6 +22,30 @@ class _TaskListDetailState extends State<TaskListDetail> {
           },
         ),
       ),
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              width: 50, // Set the width of the rectangular shape
+              height: 50, // Set the height of the rectangular shape
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(
+                    255, 205, 201, 201), // Set the background color
+                borderRadius:
+                    BorderRadius.circular(15), // Set the border radius
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'T',
+                style: TextStyle(
+                  //color: Colors.white, // Set the text color
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
